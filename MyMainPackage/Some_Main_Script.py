0e5fdf7e597eecacd5769508ad717ac288a158f1
akash-kaushik <akashkaushik041997@gmail.com>
2020-09-09 10:41:47 +0530

    My very first commit

---
+++ MyMainPackage/Some_Main_Script.py
@@ -0,0 +1,2 @@
+def report_main():
+	print("Hey I am in Some_Main_Script in Main Pack")
---
+++ myprogram.py
@@ -0,0 +1,8 @@
+from MyMainPackage import Some_Main_Script
+from MyMainPackage.SubPackage import My_Sub_Script
+
+
+Some_Main_Script.report_main()
+
+
+My_Sub_Script.sub_report()
